fe828dfe7b70176c29abd57d4f1ca9a72f2cddd5
sudesh1234 <sudeshwijesinghe1@gmail.com>
2022-05-24 11:12:21 +0530

    first

--- first/lib/pages/loginpage.dart
+++ first/lib/pages/loginpage.dart
@@ -10,6 +10,42 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      //body: SafeArea(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Text(
+              "Login",
+              style: TextStyle(
+                  fontStyle: 50.0, fontWeight: FontWeight.w700, fontSty),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.30,
+                    child: Image.asset(
+                      "assets/4786.jpg",
+                      fit: BoxFit.fitHeight,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+    //);
   }
 }
