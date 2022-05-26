9e6fef3a52cf6007b019897d12044bf9e3c1f0d1
dhvihan <danulvihan@gmail.com>
2022-05-27 01:08:56 +0530

    purchase item selecting bars

--- lib/src/ui/item_details.dart
+++ lib/src/ui/item_details.dart
@@ -9,9 +9,10 @@ class ItemDetails extends StatefulWidget {
   State<ItemDetails> createState() => _ItemDetailsState();
 }
 
-class _ItemDetailsState extends State<ItemDetails> {
+class _ItemDetailsState extends State<ItemDetails> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
+    TabController _tabController = TabController(length: 4, vsync: this);
     return Scaffold(
       appBar: AppBar(
         
@@ -68,27 +69,202 @@ class _ItemDetailsState extends State<ItemDetails> {
             Container(
                width: kPropWidth(context, 1) - 70,
                 height: kPropHeight(context, 0.42) - 252,
+                
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text("Safety Goggles",style: TextStyle(fontFamily: "poppins",fontSize: 22,fontWeight: FontWeight.w700,height: 2)),
                   Container(
                     
-                    width: kPropWidth(context, 0.3) ,
+                    width: kPropWidth(context, 0.22) ,
                     height: kPropHeight(context, 0.035),
                     decoration: BoxDecoration(
-                      
                                       borderRadius: BorderRadius.circular(4),
                                       color: Colors.blue[700]!.withOpacity(0.4),
-                                      
                                       ),
-                              child: Text("Rs.1000",style: TextStyle(fontFamily: "poppins",fontWeight:FontWeight.w500,fontSize:18),
+                              child: Text("Rs.1000",style: TextStyle(
+                                fontFamily: "poppins",
+                                fontWeight:FontWeight.w500,
+                                fontSize:19),
                               textAlign: TextAlign.center,
                   ),
                   )
                 ],
               ),
-            )
+              
+            ),
+            Container(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: TabBar(
+                      labelPadding: const EdgeInsets.only(left: 16, right: 16),
+                      controller: _tabController,
+                      labelColor: Colors.black,
+                      labelStyle: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "poppins"),
+                      unselectedLabelColor: Colors.grey,
+                      isScrollable: true,
+                      indicatorSize: TabBarIndicatorSize.label,
+                      tabs: [Tab(text: "Details"), Tab(text: "Specifications"), Tab(text: "Reviews"), Tab(text: "About Seller")],
+                    ),
+                  ),
+                ),
+             Container(
+                  height: 200,
+                  width: double.maxFinite,
+                  child: TabBarView(
+                    controller: _tabController,
+                    children: [
+                            Container(
+                        margin: EdgeInsets.only(left: 30, top: 18),
+                        child: Column(
+                          children: [
+                            Container(
+                              child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Details",
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                          fontFamily: "poppins",
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold,
+                                          height: 1.15),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.only(right: 30),
+                                      child: Text(
+                                        "See all",
+                                        textAlign: TextAlign.right,
+                                        style: TextStyle(
+                                            fontFamily: "poppins",
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.grey,
+                                            height: 1),
+                                      ),
+                                    ),
+                                  ]),
+                            ),
+                            ],
+                        ),
+                            
+                      ),
+                            Container(
+                        margin: EdgeInsets.only(left: 30, top: 18),
+                        child: Column(
+                          children: [
+                            Container(
+                              child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Spec",
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                          fontFamily: "poppins",
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold,
+                                          height: 1.15),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.only(right: 30),
+                                      child: Text(
+                                        "See all",
+                                        textAlign: TextAlign.right,
+                                        style: TextStyle(
+                                            fontFamily: "poppins",
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.grey,
+                                            height: 1),
+                                      ),
+                                    ),
+                                  ]),
+                            ),
+                            ],
+                        ),
+                            
+                      ),
+                            Container(
+                        margin: EdgeInsets.only(left: 30, top: 18),
+                        child: Column(
+                          children: [
+                            Container(
+                              child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Reviews",
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                          fontFamily: "poppins",
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold,
+                                          height: 1.15),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.only(right: 30),
+                                      child: Text(
+                                        "See all",
+                                        textAlign: TextAlign.right,
+                                        style: TextStyle(
+                                            fontFamily: "poppins",
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.grey,
+                                            height: 1),
+                                      ),
+                                    ),
+                                  ]),
+                            ),
+                            ],
+                        ),
+                            
+                      ),
+                            Container(
+                        margin: EdgeInsets.only(left: 30, top: 18),
+                        child: Column(
+                          children: [
+                            Container(
+                              child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "About",
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                          fontFamily: "poppins",
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold,
+                                          height: 1.15),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.only(right: 30),
+                                      child: Text(
+                                        "See all",
+                                        textAlign: TextAlign.right,
+                                        style: TextStyle(
+                                            fontFamily: "poppins",
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.grey,
+                                            height: 1),
+                                      ),
+                                    ),
+                                  ]),
+                            ),
+                            ],
+                        ),
+                            
+                      ),
+                            ],
+                  ),
+                )
+              
           ],
         ),
       ),
